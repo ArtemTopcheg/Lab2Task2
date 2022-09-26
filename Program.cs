60c0e0b5fd8b82b48ba84d8cba46e91aca09690f
Артём Шустов < @DESKTOP-3G11VV9>
2022-09-26 21:31:44 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Task2
+
+int[,,] nums = new int[3, 3, 3];
+Random rnd = new Random();
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        for (int k = 0; k < nums.GetLength(2); k++)
+        {
+            nums[i, j, k] = rnd.Next(-5,5) % 10;
+            if (nums[i, j, k] > 0)
+            {
+                nums[i, j, k] = 0;
+            }
+            Console.Write(nums[i, j, k] + "  ");
+        }
+    }
+    Console.WriteLine();
+}
